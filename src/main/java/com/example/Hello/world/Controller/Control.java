34c0352ab0aeb45259a3f6afbdbbc301841d880b
Praneetha-vattipalli <61209667+Praneetha-vattipalli@users.noreply.github.com>
2022-11-18 12:29:39 -0500

    hello

--- src/main/java/com/example/Hello/world/Controller/Control.java
+++ src/main/java/com/example/Hello/world/Controller/Control.java
@@ -9,6 +9,10 @@ public class Control {
     public String getHello(){
         return "hello";
     }
+    @GetMapping("/hello")
+    public String getHelloworld(){
+        return "hello";
+    }
 
 
 }
