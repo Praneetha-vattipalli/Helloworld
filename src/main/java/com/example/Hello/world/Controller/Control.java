e4d6f6a3afae4bbd00139668fd9a5e1232f4d8cd
Praneetha-vattipalli <61209667+Praneetha-vattipalli@users.noreply.github.com>
2022-11-18 12:05:32 -0500

    hi

---
+++ src/main/java/com/example/Hello/world/Controller/Control.java
@@ -0,0 +1,14 @@
+package com.example.Hello.world.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Control {
+    @GetMapping("/")
+    public String getHello(){
+        return "hello";
+    }
+
+
+}
